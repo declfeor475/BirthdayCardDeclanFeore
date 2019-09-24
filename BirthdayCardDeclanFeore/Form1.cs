e683fc22357892d23399ec5fca5c27b830b228e9
declfeor475 <declfeor475@ed.amdsb.ca>
2019-09-24 12:28:24 -0400

    finished

--- BirthdayCardDeclanFeore/Form1.cs
+++ BirthdayCardDeclanFeore/Form1.cs
@@ -30,10 +30,17 @@ namespace BirthdayCardDeclanFeore
             //graphics setting for the front of card
             Graphics g = this.CreateGraphics();
             SolidBrush cakeBrush = new SolidBrush(Color.SaddleBrown);
-            SolidBrush candleBrush = new SolidBrush(Color.CadetBlue);
+            SolidBrush candleBrush = new SolidBrush(Color.DarkOrange);
+            Pen balloonPen = new Pen(Color.CadetBlue, 5);
+            SolidBrush balloonBrush = new SolidBrush(Color.CadetBlue);
 
-            //the cake and candles 
+            //clearing the screen
+            g.Clear(Color.Black);
+
+            //the cake  
             g.FillRectangle(cakeBrush, 120, 280, 150, 60);
+
+            //the candles
             g.FillRectangle(candleBrush, 130, 240, 10, 40);
             g.FillRectangle(candleBrush, 150, 245, 10, 35);
             g.FillRectangle(candleBrush, 170, 240, 10, 40);
@@ -41,60 +48,78 @@ namespace BirthdayCardDeclanFeore
             g.FillRectangle(candleBrush, 210, 240, 10, 40);
             g.FillRectangle(candleBrush, 230, 245, 10, 35);
             g.FillRectangle(candleBrush, 250, 240, 10, 40);
+
+            //the balloons
+            g.FillEllipse(balloonBrush, 35, 20, 50, 60);
+            g.DrawLine(balloonPen, 60, 80, 60, 160);
+            g.FillEllipse(balloonBrush, 115, 50, 50, 60);
+            g.DrawLine(balloonPen, 140, 80, 140, 200);
+            g.FillEllipse(balloonBrush, 215, 50, 50, 60);
+            g.DrawLine(balloonPen, 240, 80, 240, 200);
+            g.FillEllipse(balloonBrush, 295, 20, 50, 60);
+            g.DrawLine(balloonPen, 320, 80, 320, 160);
         }
 
         private void Form1_Click(object sender, EventArgs e)
         {
             //graphics setting for the inside of the card
             Graphics g = this.CreateGraphics();
-            SolidBrush roseBrush = new SolidBrush(Color.Red);
-            Font roseFont = new Font("Comic Sans", 32);
-            SolidBrush violetBrush = new SolidBrush(Color.Blue);
-            Font violetFont = new Font("Comic Sans", 32);
+            SolidBrush greenBrush = new SolidBrush(Color.DarkSeaGreen);
+            Font greenFont = new Font("Comic Sans", 32);
             SolidBrush goldBrush = new SolidBrush(Color.Gold);
-            Font goldFont = new Font("Comic Sans", 32);
+            Font goldFont = new Font("Arial", 32);
+            SoundPlayer pageSound = new SoundPlayer(Properties.Resources.PageFlip);
+            SoundPlayer birthdaySound = new SoundPlayer(Properties.Resources.HappyBirthday);
 
-            //clear the screen
+            //flipping pages in the card
+            pageSound.Play();
+            g.Clear(Color.SlateGray);
+
+            Thread.Sleep(100);
+
+            g.Clear(Color.DarkGray);
+
+            Thread.Sleep(100);
+
+            g.Clear(Color.Gray);
+
+            Thread.Sleep(100);
+
+            g.Clear(Color.LightGray);
+
+            Thread.Sleep(100);
+
             g.Clear(Color.White);
 
-            //rose text and image
-            g.DrawString("Roses", roseFont, roseBrush, 40, 40);
+            Thread.Sleep(100);
+
+            //Today's your birthday
+            g.DrawString("Today's", greenFont, greenBrush, 40, 40);
 
             Thread.Sleep(300);
 
-            g.DrawString("Are", roseFont, roseBrush, 120, 80);
+            g.DrawString("Your", greenFont, greenBrush, 120, 85);
 
             Thread.Sleep(300);
 
-            g.DrawString("Red", roseFont, roseBrush, 180, 120);
+            g.DrawString("Birthday", greenFont, greenBrush, 180, 125);
 
-            Thread.Sleep(300);
+            Thread.Sleep(1000);
 
-            //violet text and image
-            g.DrawString("Violets", violetFont, violetBrush, 30, 120);
-
-            Thread.Sleep(300);
-
-            g.DrawString("Are", violetFont, violetBrush, 130, 160);
-
-            Thread.Sleep(300);
-
-            g.DrawString("Blue", violetFont, violetBrush, 180, 200);
-
-            Thread.Sleep(300);
-            
-            //violet text and image
+            //Happy birthday to ya!
             g.DrawString("Happy", goldFont, goldBrush, 40, 200);
 
             Thread.Sleep(300);
 
-            g.DrawString("Birthday", goldFont, goldBrush, 120, 240);
+            g.DrawString("Birthday", goldFont, goldBrush, 120, 245);
 
             Thread.Sleep(300);
 
-            g.DrawString("To You!", goldFont, goldBrush, 160, 280);
+            g.DrawString("To Ya!", goldFont, goldBrush, 180, 290);
 
             Thread.Sleep(300);
+
+            birthdaySound.Play();
         }
     }
 }
